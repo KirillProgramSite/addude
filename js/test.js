41f81b191170ce7d39166290ae5c6737dabcfd71
Kirill Proskurin <78263182+KirillProgramSite@users.noreply.github.com>
2023-03-08 20:33:08 +0300

    Add files via upload

--- js/test.js
+++ js/test.js
@@ -3,20 +3,35 @@ let linkRes = document.querySelector('.res-link')
 function result() {
   let resLet = '';
 
-  const obj = document.querySelector('input[name="obj"]:checked').value;
-  const work = document.querySelector('input[name="work"]:checked').value;
-  const games = document.querySelector('input[name="games"]:checked').value;
-  const attempt = document.querySelector('input[name="attempt"]:checked').value;
+  const test1 = document.querySelector('input[name="test1"]:checked').value;
+  const test2 = document.querySelector('input[name="test2"]:checked').value;
+  const test3 = document.querySelector('input[name="test3"]:checked').value;
+  const test4 = document.querySelector('input[name="test4"]:checked').value;
+  const test5 = document.querySelector('input[name="test5"]:checked').value;
+  const test6 = document.querySelector('input[name="test6"]:checked').value;
+  const test7 = document.querySelector('input[name="test7"]:checked').value;
+  const test8 = document.querySelector('input[name="test8"]:checked').value;
+  const test9 = document.querySelector('input[name="test9"]:checked').value;
+  const test10 = document.querySelector('input[name="test10"]:checked').value;
+  const test11 = document.querySelector('input[name="test11"]:checked').value;
+  const test12 = document.querySelector('input[name="test12"]:checked').value;
+  const test13 = document.querySelector('input[name="test13"]:checked').value;
+  const test14 = document.querySelector('input[name="test14"]:checked').value;
+  const test15 = document.querySelector('input[name="test15"]:checked').value;
 
 
-  if (obj == "1" && work == "1" && games == "3" && attempt == "1") {
+
+  if (test1, test2, test3, test4, test5, test6, test7, test8, test9, test10, test11, test12, test13, test14, test15 == "1") {
       resLet = 'tech'
+  } else if(test1, test2, test3, test4, test5, test6, test7, test8, test9, test10, test11, test12, test13, test14, test15 == "2") {
+    resLet = 'guman'
+  } else if(test1, test2, test3, test4, test5, test6, test7, test8, test9, test10, test11, test12, test13, test14, test15 == "3") {
+    resLet = 'bis'
   } else {
-      resLet = 'bio'
+    resLet = 'other'
   }
 
   localStorage.setItem('reslet', resLet)
-  console.log(obj, work, games, attempt);
 }
 
 linkRes.addEventListener('click', result)
